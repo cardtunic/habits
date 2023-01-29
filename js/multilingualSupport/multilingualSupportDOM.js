6903c0b32339bce027b63f35d7c0981ed1dc80eb
Kaleb Mendes <cardtunic@gmail.com>
2023-01-29 15:49:03 -0300

    :sparkles: feat: add language selection menu

--- js/multilingualSupport/multilingualSupportDOM.js
+++ js/multilingualSupport/multilingualSupportDOM.js
@@ -1,41 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  setLangaugeText();
+  addLangaugeText();
 });
 
-async function setLangaugeText() {
-  const currentLangauge = await getCurrentLanguage();
-  const elementsToSetText = document.querySelectorAll("[data-translate-key]");
+// adiciona o texto dos elementos HTML da página, de acordo com a linguagem atual
 
-  elementsToSetText.forEach((element) => {
+async function addLangaugeText() {
+  const currentLangauge = await getLanguage(); // retorna a linguagem atual
+  const elementsToAddText = document.querySelectorAll("[data-translate-key]"); // retorna os elementos que terão texto adicionado
+
+  // passa por todos os elementos e adiciona o texto relativo ao data-translate-key
+
+  elementsToAddText.forEach((element) => {
     const translateKey = element.getAttribute("data-translate-key");
 
-    if (typeof currentLangauge[translateKey] === "object") {
-      const fatherObject = currentLangauge[translateKey];
+    // dispara se o elemento estiver dentro de outro objeto
 
-      Object.keys(fatherObject).forEach((key) => {
-        const elementText = fatherObject[key];
+    if (typeof currentLangauge[translateKey] === "object") {
+      const parentObjetct = currentLangauge[translateKey];
+
+      // passa por cada "key" do objeto pai, e adiciona o texto relativo ao elemento
+
+      Object.keys(parentObjetct).forEach((key) => {
+        const elementText = parentObjetct[key]; // retorna o texto relativo ao elemento
+
+        // dispara se o elemento for um input, mudando seu placeholder
 
         if (key === "input") {
-          element
-            .querySelector(key)
-            .setAttribute(
-              "placeholder",
-              elementText.replace("{{child}}", element.innerHTML)
-            );
+          element.querySelector(key).setAttribute("placeholder", elementText);
 
           return;
         }
 
+        const elementChildren = element.querySelector(key).innerHTML; // retorna o HTML interno, com os filhos do elemento
+
+        // adiciona o texto, mas sem alterar o HTML interno anterior
+
         element.querySelector(key).innerHTML = elementText.replace(
           "{{child}}",
-          element.innerHTML
+          elementChildren
         );
       });
 
       return;
     }
 
-    const elementText = currentLangauge[translateKey];
-    element.innerHTML = elementText.replace("{{child}}", element.innerHTML);
+    const elementText = currentLangauge[translateKey]; // retorna o texto relativo ao elemento
+    const elementChildren = element.innerHTML; // retorna o HTML interno, com os filhos do elemento
+
+    element.innerHTML = elementText.replace("{{child}}", element.innerHTML); // adiciona o texto, mas sem alterar o HTML interno anterior
   });
 }
--- js/multilingualSupport/multilingualSupportRepository.js
+++ js/multilingualSupport/multilingualSupportRepository.js
@@ -1,28 +1,56 @@
-function setUserLanguagePreference(languageCode) {
-  localStorage.setItem("lang", languageCode);
+// define a preferência de linguagem do usuário
+
+function setUserLanguagePreference(languageTag) {
+  localStorage.setItem("lang", languageTag);
 }
 
-async function getCurrentLanguage() {
+// retorna uma lista com as linguagem disponíveis
+
+async function getAvailableLanguages() {
   const availabeLangs = await (
     await fetch("../../assets/repos/langs/availableLangs.json")
   ).json();
 
-  const userLanguagePreference = localStorage.getItem("lang");
-  let userLanguageCode = userLanguagePreference || navigator.language;
+  return availabeLangs;
+}
 
-  if (!availabeLangs.includes(userLanguageCode)) {
-    if (userLanguageCode.length === 2) {
-      userLanguageCode = availabeLangs.filter((lang) =>
-        lang.startsWith(userLanguageCode)
+// retorna a linguagem de preferência do usuário
+
+async function getUserLanguageTag() {
+  const availabeLangs = await getAvailableLanguages();
+  const userLanguagePreference = localStorage.getItem("lang");
+
+  let userLanguageTag = userLanguagePreference || navigator.language;
+  // define o código de linguagem da página com base na preferência do usuário, somente caso ela já estiver definida
+  // caso contrário, pega a linguagem do navegador do usuário
+
+  // dispara se a tag da linguagem não corresponde a nenhuma das disponíveis
+
+  if (!availabeLangs.includes(userLanguageTag)) {
+    // dispara se a linguagem do usuário não obedecer ao padrão: "linguagem-país" (ex.: pt-BR)
+
+    if (userLanguageTag.length === 2) {
+      userLanguageTag = availabeLangs.filter((lang) =>
+        lang.startsWith(userLanguageTag)
       )[0];
+
+      // define a linguagem padrão como sendo inglês, caso a do usuário não esteja disponível
     } else {
-      userLanguageCode = "en-US";
+      userLanguageTag = "en-US";
     }
   }
 
-  const currentLangauge = await (
-    await fetch(`../../assets/repos/langs/${userLanguageCode}.json`)
+  return userLanguageTag;
+}
+
+// retorna o objeto da linguagem atual, com todos as strings necessárias
+
+async function getLanguage() {
+  const userLanguageTag = await getUserLanguageTag();
+
+  const language = await (
+    await fetch(`../../assets/repos/langs/${userLanguageTag}.json`)
   ).json();
 
-  return currentLangauge;
+  return language;
 }
